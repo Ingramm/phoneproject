5081b8112566227986d517763be71ba036430259
lxc <2585162033@qq.com>
2018-11-20 09:54:27 +0800

    Initial commit

---
+++ src/main/java/com/pp/phoneproject/PhoneprojectApplication.java
@@ -0,0 +1,12 @@
+package com.pp.phoneproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoneprojectApplication.class, args);
+    }
+}
